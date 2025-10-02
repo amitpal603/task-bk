e2e526373d4763f6d54e1abf4730059691ee5d0b
amitpal603-ops <ap036776@gmail.com>
2025-10-03 01:39:23 +0530

    task complete

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const TaskRouters = require('./routers/TaskRouters')
+const cors = require('cors')
+
+const corsOptions = {
+    origin :' http://localhost:5173',
+    methods : ["GET","POST","PUT","DELETE"],
+    credential : true
+    
+}
+const app = express()
+connectDB()
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/task',TaskRouters)
+
+app.get('/',(req,res) => {
+ res.send('Hello')
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT,() => {
+    console.log(`server listen or running http://localhost:${PORT}`);
+    
+})
