51f5916383cf641a2bb753c19d5cfb04e14ed04b
amitpal603-ops <ap036776@gmail.com>
2025-10-22 23:14:46 +0530

    deploy on render

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = async() => {
-    try {
-        mongoose.connect(process.env.MONGODB_URI,{
-             useNewUrlParser: true,
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
       tls: true,
-      tlsAllowInvalidCertificates: true, 
-        })
-        console.log(' Database connected successFully.. ');
-        
-    } catch (error) {
-        console.log(error.message);
-        
-    }
-}
-module.exports = connectDB
+      tlsAllowInvalidCertificates: true,
+    });
+    console.log(" Database connected successFully.. ");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+module.exports = connectDB;
